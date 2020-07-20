7e34108f6b029e2c8191b3c79b0428af727dd9e8
Khoi <KHoigrai1@gmail.com>
2020-07-20 20:09:24 +0700

    Console_setup (80W 50H)
    set_console_size
    
    set_map_size
    
    const_console_size
    
     ignore_mouse
    
    console_set_up
    
    void hind_roll

---
+++ Project-Group-8/main/Console_size.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <windows.h>
+#include<iostream>
+
+using namespace std;
+
+// set size console
+void set_console_size(SHORT width, SHORT height);
+
+// set size map
+void set_map_size(SHORT width, SHORT height);
+
+void const_console_size();
+
+void ignore_mouse();
+
+void console_set_up(SHORT width, SHORT height);
+
+void hind_roll(BOOL Show);
